be218dfa1d2adaa9986e0bb08e7957db272a8fbe
Laith alnatour <laithalnatour92@gmail.com>
2022-08-08 22:35:47 +0300

    solve lap15

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
+import './App.css';
+
 
 class Footer extends React.Component {
   render() {
     return (
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand>Code Fellows</Navbar.Brand>
+      <Navbar collapseOnSelect >
+        <Navbar.Brand> <br/><br/><br/><br/> Laith Alnatour</Navbar.Brand>
       </Navbar>
     )
   }
